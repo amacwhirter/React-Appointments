04e5880984b42b446bc6b439d85e4028dc32102a
Ashley MacWhirter <macwhirtera@gmail.com>
2017-07-18 00:02:43 -0400

    save changes

--- app/client/Components/Search.js
+++ app/client/Components/Search.js
@@ -9,6 +9,66 @@ import css from "../../public/css/main.scss";
 
 class Search extends React.Component {
 
+  constructor() {
+      super();
+
+      this.state = {
+          showresult: false,
+          data: []
+      };
+
+      this.getData = this.getData.bind(this);
+      this.showResult = this.showResult.bind(this);
+      this.searchParent = this.searchParent.bind(this);
+
+  }
+
+  getData(event) {
+      event.preventDefault();
+
+      var data;
+
+      for (var field in this.refs) {
+          data = this.refs[field].value;
+
+          console.log(data);
+          this.refs[field].value = '';
+      }
+
+      helper.getAppointments(data).then(function(res) {
+          this.props.setParent(res.data);
+
+          console.log(res.data.length);
+
+          if (res.data.length <= 0) {
+              this.props.found(true);
+              console.log(this.state.data);
+              this.props.setParent(this.state.data);
+          }
+      }.bind(this)).catch(function(err) {
+          console.log(err);
+
+      }.bind(this));
+
+  }
+
+  showResult() {
+      if (this.state.showresult) {
+          this.setState({showresult: false});
+      } else {
+          this.setState({showresult: true});
+      }
+  }
+
+  componentDidMount() {
+      //this.setState({data: this.props.results});
+      console.log(this.props.results);
+  }
+
+  searchParent(data) {
+      this.props.setParent(data);
+      this.setState({data: data});
+  }
 
     render() {
         return (
--- app/client/Components/Table.js
+++ app/client/Components/Table.js
@@ -8,24 +8,21 @@ import css from "../../public/css/main.scss";
 
 class TableAppointments extends Component {
 
-	constructor() {
-			super();
-			this.state = { data: [] }
-	}
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        }
+    }
 
-	componentDidMount(){
-      helper.getData()
-          .then(function (res) {
-              this.setState({data: res.data});
-              console.log(this.state.data);
-          }.bind(this))
-
-          .catch(function (err) {
-              console.log(err);
-          }.bind(this));
-  }
-
-
+    componentDidMount() {
+        helper.getData().then(function(res) {
+            this.setState({data: res.data});
+            console.log(this.state.data);
+        }.bind(this)).catch(function(err) {
+            console.log(err);
+        }.bind(this));
+    }
 
     render() {
 
@@ -42,18 +39,18 @@ class TableAppointments extends Component {
                         </tr>
                     </thead>
 
-										<tbody>{this.state.data.map(function() {
+                    <tbody>{this.state.data.map(function() {
 
-							               return (
-							                  <tr key = {this.state.id}>
-							                      <td>{this.state.id}</td>
-							                      <td>{this.state.date}</td>
-							                      <td>{this.state.time}</td>
-							                      <td>{this.state.description}</td>
-							                  </tr>
-							                )
+                            return (
+                                <tr key={this.state.id}>
+                                    <td>{this.state.id}</td>
+                                    <td>{this.state.date}</td>
+                                    <td>{this.state.time}</td>
+                                    <td>{this.state.description}</td>
+                                </tr>
+                            )
 
-							             })}</tbody>
+                        })}</tbody>
                 </Table>
             </div>
         );
